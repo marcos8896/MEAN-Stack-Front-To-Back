b502c73e8627e63f5abd2930a6f9ea983ec2337c
marcos8896 <elyomarcos@gmail.com>
2017-09-08 23:57:42 -0600

    MEAN Stack Front To Back [Part 8] - Login & Logout

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -11,17 +11,39 @@ export class AuthService {
   user: any;
   urlConfig: any;
   url: string;
+  headers: Headers;
 
-  constructor(private http: Http, private ConfigUrlService: ConfigUrlService) { 
+  constructor(private http: Http, private ConfigUrlService: ConfigUrlService) {
+    
+    this.headers = new Headers();
+    this.headers.append('Content-Type', 'application/json');
+
     this.urlConfig = this.ConfigUrlService.getConfig();
     this.url = `${this.urlConfig.protocol}://${this.urlConfig.host}:${this.urlConfig.port}`;
   }
 
   registerUser(user){
-    let headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-    return this.http.post(`${this.url}/users/register`, user, {headers: headers})
+    return this.http.post(`${this.url}/users/register`, user, {headers: this.headers})
       .map(res => res.json());
+  }
+
+  authenticateUser(user){
+    return this.http.post(`${this.url}/users/authenticate`, user, {headers: this.headers})
+    .map(res => res.json());
+
+  }
+
+  storeUserData(token, user){
+    localStorage.setItem('id_token', token);
+    localStorage.setItem('user', JSON.stringify(user));
+    this.authToken = token;
+    this.user = user;
+  }
+
+  logout(){
+    this.authToken = null;
+    this.user = null;
+    localStorage.clear();
   }
 
 }
